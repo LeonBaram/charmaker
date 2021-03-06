5dbd4c637e7b5312394632427756549a332b71cf
LeonBaram <leonbaram@protonmail.com>
2021-03-06 16:30:27 -0500

    moved non-component js files into data folder

---
+++ src/data/descriptions.js
@@ -0,0 +1,22 @@
+const descriptions = {
+    race: {
+        human: "",
+        halfling: "",
+        elf: "",
+        dwarf: "",
+    },
+    dndclass: {
+        fighter: "",
+        rogue: "",
+        wizard: "",
+        cleric: "",
+    },
+    background: {
+        soldier: "",
+        criminal: "",
+        sage: "",
+        acolyte: ""
+    }
+};
+
+export default descriptions;
