2a4d88a4ade0b400a55833588d1cf0d1c54211d5
LeonBaram <leonbaram@protonmail.com>
2021-10-11 20:46:13 -0400

    set up typescript and firebase

---
+++ pages/data/firebase.ts
@@ -0,0 +1,29 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// const pathref = path => firebase.database().ref(path);
+const dbref = (path: string) => ref(db, path);
+
+// const dbSections = [
+//   'characters',
+// ];
+
+// const dbref = {root: pathref()};
+// for (let section of dbSections) {
+//   dbref[section] = pathref(section);
+// }
+
+export { dbref };
