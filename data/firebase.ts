0527ad50c93697cf14c18c385a8f19e5faacb3ad
LeonBaram <leonbaram@protonmail.com>
2021-10-12 16:08:27 -0400

    Move firebase init script out of pages folder

--- data/firebase.ts
+++ data/firebase.ts
@@ -4,7 +4,7 @@ import { getDatabase, ref, set } from "firebase/database";
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DB_URL,
+  databaseURL: "https://portfolio-c648b-default-rtdb.firebaseio.com",
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
